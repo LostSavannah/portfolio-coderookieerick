04e91a31b064d26f87916d13f33b8416674ded41
erickmoradev@gmail.com <erickfernandomoraramirez@gmail.com>
2021-11-07 12:12:33 -0400

    'idk'

--- habilities.php
+++ habilities.php
@@ -1,82 +1,79 @@
-<?php 
-    session_start(); 
-    $pageName = "Mis habilidades";
-    require('connection.php');
+<?php
+session_start();
+$pageName = "Mis habilidades";
+require('connection.php');
 ?>
 
 <!Doctype html>
 <html>
-    <?php include('./res/parts/head.php');?>
-    <body>
-        <?php
-            //print_r($_SERVER);
-            //phpinfo();
-            //print(PHP_BINDIR);
-            include('./res/parts/header.php'); 
-            include('./res/parts/nav.php'); 
-            include('./res/parts/subHeader.php');
+<?php include('./res/parts/head.php'); ?>
+
+<body>
+    <?php
+    include('./res/parts/header.php');
+    include('./res/parts/nav.php');
+    include('./res/parts/subHeader.php');
+    ?>
+    <div class="container">
+        <?PHP
+        $data = query_as_array("select * from habilities;", "habilities.db");
+        //print_r($data);
+        $c = 0;
+
         ?>
-        <div class="container">
-            <?PHP
-                $data = query_as_array("select * from habilities;", "habilities.db");
-                //print_r($data);
-                $c = 0;
-                
-            ?>
-            <div class="row">
-                <div class="col-12 h5">
-                    A continuaci&oacute;n estan algunas de mis habilidades:
-                </div>
+        <div class="row">
+            <div class="col-12 h5">
+                A continuaci&oacute;n estan algunas de mis habilidades:
             </div>
-            <?PHP foreach($data as $dt):?>
-            
-            <div class="row bio-card"
-                    style="<?PHP echo 'border-left: solid 0.3em '.$dt['background'];?>;"
-            >
-                <div class="col-12 h6"
-                    style="<?PHP echo 'border-left: solid 0.3em '.$dt['background'];?>;"
-                >
+        </div>
+        <?PHP foreach ($data as $dt) : ?>
+
+            <div class="row bio-card" style="<?PHP echo 'border-left: solid 0.3em ' . $dt['background']; ?>;">
+                <div class="col-12 h6" style="<?PHP echo 'border-left: solid 0.3em ' . $dt['background']; ?>;">
                     <?PHP
-                        echo sprintf('<img style="width:2em; height:2em;" src="%s"/>', $dt['icon']); 
-                        echo $dt['topic'];
+                    echo sprintf('<img style="width:2em; height:2em;" src="%s"/>', $dt['icon']);
+                    echo $dt['topic'];
                     ?>
                 </div>
                 <?PHP
-                    $c+=1;
-                    $habilityId = $dt['id'];
-                    $query = sprintf("select * from HabilitiesExamples where idHability = %d;", $habilityId);
-                    $examples = query_as_array($query, "habilities.db");
+                $c += 1;
+                $habilityId = $dt['id'];
+                $query = sprintf("select * from HabilitiesExamples where idHability = %d;", $habilityId);
+                $examples = query_as_array($query, "habilities.db");
 
-                    ?>
-                    <div class="col-12 col-md-6">   
-                        <p><?PHP echo $dt['title'];?></p>
-                        <p><?PHP echo $dt['habilityDescription'];?></p>
-                    </div>
-                    <div class="col-12 col-md-6 text-center"><ul>
-                    <?PHP if($examples):?>
-                        <div class="h6">Proyectos</div>
-                    <?PHP endif;?>
-                    <?PHP foreach($examples as $example){
-                        printf(
-                            '<div><a href="%s">%s</a></div>',
-                            $example['habilityViewUrl'],
-                            $example['title']
+                ?>
+                <div class="col-12 col-md-6">
+                    <p><?PHP echo $dt['title']; ?></p>
+                    <p><?PHP echo $dt['habilityDescription']; ?></p>
+                </div>
+                <div class="col-12 col-md-6 text-center">
+                    <ul>
+                        <?PHP if ($examples) : ?>
+                            <div class="h6">Proyectos</div>
+                        <?PHP endif; ?>
+                        <?PHP foreach ($examples as $example) {
+                            printf(
+                                '<div><a href="%s">%s</a></div>',
+                                $example['habilityViewUrl'],
+                                $example['title']
                             );
-                            }?>
-                    </ul></div>
+                        } ?>
+                    </ul>
+                </div>
             </div>
-            <?PHP endforeach;?>
-            <?PHP
-                include("./res/parts/footer.php");
-            ?>
-        </div>
-        <?php
-            
+        <?PHP endforeach; ?>
+        <?PHP
+        include("./res/parts/footer.php");
         ?>
-    </body>
-    <script>    
-        $(document).ready(()=>{
+    </div>
+    <?php
 
-        });
-    </script>
+    ?>
+</body>
+<script>
+    $(document).ready(() => {
+
+    });
+</script>
+
 </html>
